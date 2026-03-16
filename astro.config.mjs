b4d3f142c7aece57da7be79320927fde64614811
Micci Curto <miccicurto@Miccis-Mac-mini.local>
2026-03-16 10:54:07 -0400

    fixed bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [tailwind(), mdx()],
 });
